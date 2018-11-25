240604c3934a6faa5ad6b48c6a35dcea547a610e
Tomáš Virtus <nechtom@gmail.com>
2018-11-25 21:32:34 +0100

    OPS-369: Use unique image names on Drone CI
    Nothing should change for local development except that instead of
    coop.rchain/rnode:0.7.1, coop.rchain/rnode:0.7.1-git${SHORTREV} is
    created where ${SHORTREV} is first 8 characters of top commit. If the
    build is not run from Git repository, -git suffix is not appended.

--- integration-testing/run_tests.sh
+++ integration-testing/run_tests.sh
@@ -1,4 +1,13 @@
-#!/bin/bash -ue
+#!/bin/bash -e
 
-docker build --tag rchain-integration-testing .
+if [[ ! -f Dockerfile.tmp || Dockerfile.in -nt Dockerfile.tmp ]]; then
+	if [[ -n $DRONE_BUILD_NUMBER ]]; then
+		tag=DRONE-$DRONE_BUILD_NUMBER
+	else
+		tag=latest
+	fi
+	sed "s/@@TAG@@/$tag/" Dockerfile.in >Dockerfile.tmp
+fi
+
+docker build --tag rchain-integration-testing -f Dockerfile.tmp .
 pipenv run py.test -v "$@"
